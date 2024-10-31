f40c54b274dc2a098f5cab6b26f92dbb1ca306a9
kietpt2003 <tuankiet29012003@gmail.com>
2024-10-31 14:36:33 +0700

    Update notification demo example

--- notification-test/src/Notification.js
+++ notification-test/src/Notification.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from 'react';
 import * as signalR from "@microsoft/signalr"
 
 const Notifications = () => {
-    const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6IntcIklkXCI6XCI1YTU3MjIzYS02ZTdkLTQwMWItYTE5ZS1iZjkyODJkYjY5ZmVcIixcIkVtYWlsXCI6XCJjdXN0b21lcjFAZ21haWwuY29tXCIsXCJMb2dpbk1ldGhvZFwiOlwiRGVmYXVsdFwiLFwiUm9sZVwiOlwiQ3VzdG9tZXJcIixcIlN0YXR1c1wiOlwiQWN0aXZlXCJ9IiwiVG9rZW5DbGFpbSI6IkZvclZlcmlmeU9ubHkiLCJuYmYiOjE3MzAzMDA3MzMsImV4cCI6MTczMDMwMjUzMywiaWF0IjoxNzMwMzAwNzMzLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUyNDYiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUyNDYifQ.0zUBU2TcFrGfhH0fbtgJmbjO6_xDZ4xflSbDDXXoa2U";
+    const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6IntcIklkXCI6XCI1YTU3MjIzYS02ZTdkLTQwMWItYTE5ZS1iZjkyODJkYjY5ZmVcIixcIkVtYWlsXCI6XCJjdXN0b21lcjFAZ21haWwuY29tXCIsXCJMb2dpbk1ldGhvZFwiOlwiRGVmYXVsdFwiLFwiUm9sZVwiOlwiQ3VzdG9tZXJcIixcIlN0YXR1c1wiOlwiQWN0aXZlXCJ9IiwiVG9rZW5DbGFpbSI6IkZvclZlcmlmeU9ubHkiLCJuYmYiOjE3MzAzNjAwODUsImV4cCI6MTczMDM2MTg4NSwiaWF0IjoxNzMwMzYwMDg1LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUyNDYiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUyNDYifQ.CzsZcMz03cViRarb1OxiZLvPMNU5gYHLls7fsRpvE_k";
     const [connection, setConnection] = useState(null);
     const [message, setMessage] = useState('');
+    const [message2, setMessage2] = useState('');
 
     useEffect(() => {
         // Create the SignalR connection
         const newConnection = new signalR.HubConnectionBuilder()
-            .withUrl(`https://localhost:7046/notification/hub?access_token=${accessToken}`, { withCredentials: false })
+            .withUrl(`https://tech-gadgets-dev.xyz/notification/hub?access_token=${accessToken}`, {
+                withCredentials: false
+            })
             .withAutomaticReconnect()
             .build();
 
@@ -17,9 +20,16 @@ const Notifications = () => {
         newConnection.start()
             .then(() => {
                 console.log('SignalR Connected!');
-                // You can send a message to the server here if needed
-                newConnection.on('ReceiveMessage', (user, receivedMessage) => {
+                // Call the restricted method "SendMessage"
+                newConnection.invoke("JoinGroup", "CustomerGroup")
+                    .then(() => console.log("JoinGroup method invoked successfully"))
+                    .catch(err => console.error("Error invoking JoinGroup method:", err));
+                newConnection.on('GroupMethod', (user, receivedMessage) => {
                     setMessage(`${user}: ${receivedMessage}`);
+                });
+                // Call the restricted method "PersonalMessage"
+                newConnection.on('PersonalMethod', (user, receivedMessage) => {
+                    setMessage2(`${user}: ${receivedMessage}`);
                 });
             })
             .catch(err => console.log('SignalR Connection Error: ', err));
@@ -37,7 +47,8 @@ const Notifications = () => {
     return (
         <div>
             <h1>SignalR Messages</h1>
-            <p>{message}</p>
+            <p>Group: {message}</p>
+            <p>Personal: {message2}</p>
         </div>
     );
 };
